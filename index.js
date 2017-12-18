79d6519f1c11f33bf14ed8ec876874b7a4d0dccd
Lauren Schwartz <lschwartzwriting@gmail.com>
2017-12-18 21:23:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return(newObj);
 }
 
-function destructivelyUpdateObjectWithKeyAndValue()
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return(object);
+}
